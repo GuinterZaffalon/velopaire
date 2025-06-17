206ccd6d44a96f19b357fcd1932bfba98f464d9b
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2025-06-17 14:11:03 -0300

    feat: app rodando agora e tela na horizontal

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class HomeScreen extends StatefulWidget{
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]).then((_) {
+    runApp(const HomeScreen());
+  });
+}
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>{
-
-
-  @override
-void initState(){
-  super.initState();
-  SystemChrome.setPreferredOrientations([
-      DeviceOrientation.landscapeRight,
-      DeviceOrientation.landscapeLeft,
-  ]);
-}
-
+class _HomeScreenState extends State<HomeScreen> {
+  
 
   @override
-  Widget build(BuildContext build){
-    return Container();
+  Widget build(BuildContext build) {
+    return Container(
+      color: Colors.white,
+      height: double.infinity,
+      width: double.infinity,
+    );
   }
 }
