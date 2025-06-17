3f1493ccfba12db38bbf0a989ece272b188e66d0
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2025-06-17 15:16:36 -0300

    mapa funcionando!!!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -7,7 +10,11 @@ void main() {
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
   ]).then((_) {
-    runApp(const HomeScreen());
+    runApp(
+      const MaterialApp(
+        home: HomeScreen(),
+      ),
+    );
   });
 }
 
@@ -19,27 +26,81 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  GoogleMapController? _mapController;
+  LatLng? _userLocation;
+
   @override
-  Widget build(BuildContext build) {
-    return Container(
+  void initState() {
+    super.initState();
+    _getUserLocation();
+  }
+
+  Future<void> _getUserLocation() async {
+    await Permission.location.request();
+
+    if (await Permission.location.isGranted) {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+      setState(() {
+        _userLocation = LatLng(position.latitude, position.longitude);
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Permissão de localização negada')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(10),
         decoration: BoxDecoration(
-      boxShadow: [
-        BoxShadow(
-          color: Colors.grey.withValues(alpha: 0.3),
-          spreadRadius: 2,
-          blurRadius: 7,
-          offset: Offset(0, 3),
+          gradient: const LinearGradient(
+            begin: Alignment.centerRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color.fromRGBO(255, 72, 0, 1),
+              Color.fromRGBO(42, 42, 89, 1),
+            ],
+          ),
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 2,
+              blurRadius: 7,
+              offset: const Offset(0, 3),
+            ),
+          ],
         ),
-      ],
-      gradient: LinearGradient(
-        begin: Alignment.centerRight,
-        end: Alignment.bottomLeft,
-        colors: [
-          Color.fromRGBO(255, 72, 0, 1),
-          Color.fromRGBO(42, 42, 89, 1),
-        ],
+        child: Row(
+          children: [
+            Expanded(
+              child: _userLocation == null
+                  ? const Center(child: CircularProgressIndicator())
+                  : GoogleMap(
+                      initialCameraPosition: CameraPosition(
+                        target: _userLocation!,
+                        zoom: 16,
+                      ),
+                      myLocationEnabled: true,
+                      myLocationButtonEnabled: true,
+                      onMapCreated: (controller) => _mapController = controller,
+                      markers: {
+                        Marker(
+                          markerId: const MarkerId('user_location'),
+                          position: _userLocation!,
+                          infoWindow: const InfoWindow(title: 'Você está aqui'),
+                        ),
+                      },
+                    ),
+            ),
+          ],
+        ),
       ),
-      borderRadius: BorderRadius.circular(8),
-    ));
+    );
   }
 }
