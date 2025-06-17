a9b0c6d8346d72279a026dca3020ee2de52eaa50
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2025-06-17 14:41:23 -0300

    feat: .env e mais libs!

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
@@ -20,30 +19,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  
-
   @override
   Widget build(BuildContext build) {
     return Container(
-                          decoration: BoxDecoration(
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withValues(alpha: 0.3),
-                                spreadRadius: 2,
-                                blurRadius: 7,
-                                offset:
-                                    Offset(0, 3), // changes position of shadow
-                              ),
-                            ],
-                            gradient: LinearGradient(
-                              begin: Alignment.centerRight,
-                              end: Alignment.bottomLeft,
-                              colors: [
-                                Color.fromRGBO(255, 72, 0, 1),
-                                Color.fromRGBO(42, 42, 89, 1),
-                              ],
-                            ),
-                            borderRadius: BorderRadius.circular(8),
-                          ));
+        decoration: BoxDecoration(
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withValues(alpha: 0.3),
+          spreadRadius: 2,
+          blurRadius: 7,
+          offset: Offset(0, 3),
+        ),
+      ],
+      gradient: LinearGradient(
+        begin: Alignment.centerRight,
+        end: Alignment.bottomLeft,
+        colors: [
+          Color.fromRGBO(255, 72, 0, 1),
+          Color.fromRGBO(42, 42, 89, 1),
+        ],
+      ),
+      borderRadius: BorderRadius.circular(8),
+    ));
   }
 }
